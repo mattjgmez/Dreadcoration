a17806d5773106b6d77dd2b6c89e93b572dc01af
TheJadePhoenix <mattgmez98@gmail.com>
2024-03-06 23:49:15 -0500

    Reorganized script folders
    Also removed ugly outline of shotgun_UI

--- scripts/managers/mouse_manager.gd
+++ scripts/managers/mouse_manager.gd
@@ -4,10 +4,7 @@ const CAPTURED := Input.MOUSE_MODE_CAPTURED
 const RELEASED := Input.MOUSE_MODE_VISIBLE
 
 var gameplay_active := true
-var screen_resolutions := [
-	Vector2(1920, 1080),
-	Vector2(860, 520)
-]
+
 
 func _ready():
 	Input.mouse_mode = CAPTURED
@@ -21,9 +18,3 @@ func _input(event):
 	if event.is_action_pressed("gameplay_action") and gameplay_active:
 		if Input.mouse_mode == RELEASED:
 			Input.mouse_mode = CAPTURED
-	
-	if event.is_action_pressed("gameplay_interact"):
-		var next_res = screen_resolutions.pop_back()
-		get_tree().root.content_scale_size = next_res
-		screen_resolutions.push_front(next_res)
-		print("Resolution set to:", next_res.x, "/", next_res.y)
