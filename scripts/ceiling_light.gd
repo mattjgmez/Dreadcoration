f4c2aeae006cae652f4ac00e14abf321d4c94a91
TheJadePhoenix <mattgmez98@gmail.com>
2024-03-08 21:09:21 -0500

    Added animation states to Ceiling Lights

---
+++ scripts/ceiling_light.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+
+@export_range(0.0, 1.0, 0.01) var flicker_chance := 0.0
+@export var start_on := true
+
+@onready var animator := $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if start_on:
+		turn_on()
+
+
+func turn_on():
+	var random_float = randf()
+	
+	if random_float <= flicker_chance:
+		animator.play("flicker")
+	else:
+		animator.play("on")	
